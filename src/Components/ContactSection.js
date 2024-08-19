0aa7ff157120a0ebbaa15e1cd79cb638aa87856e
Hasan Raza <95583135+AhsanJay@users.noreply.github.com>
2024-08-19 06:35:40 +0500

    Portfolio

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -14,7 +14,7 @@ const ContactSection = () => {
         setSuccess('');
 
         try {
-            await axios.post('http://localhost:5000/send-email', formData);
+            await axios.post('portfolio-backend-production-e1af.up.railway.app/send-email', formData);
             setSuccess('Message sent successfully');
         } catch (error) {
             setError('Failed to send message');
